dde6de1825e0ea4b27ceb27d93776569a7f1b99a
tofuru382 <tomoharu.furuya@gmail.com>
2025-09-30 13:39:08 +0900

    Add files via upload

--- worker.js
+++ worker.js
@@ -3,7 +3,7 @@ export default {
     if (request.method === "OPTIONS") {
       return new Response(null, {
         headers: {
-          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Origin": "railbuddy.pages.dev",
           "Access-Control-Allow-Methods": "POST, OPTIONS",
           "Access-Control-Allow-Headers": "Content-Type",
         },
